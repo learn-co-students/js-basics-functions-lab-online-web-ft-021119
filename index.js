33fdabbc258d4d666a2a25f026962612d42ecef6
Christoph Gruener <christophgruener@me.com>
2019-05-22 16:37:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+  if (street > 42) {
+    let blocks = 42 - street
+    blocks = ~blocks + 1
+    return blocks
+  } else {
+    return 42 - street
+  }
+}
+
+function distanceFromHqInFeet(street) {
+  let blocks = distanceFromHqInBlocks(street);
+  return blocks*264
+}
+
+function distanceTravelledInFeet(pickup, destination) {
+    let distance;
+    if (pickup > destination) {
+      distance = (pickup - destination);
+    } else {
+      distance = (destination - pickup);
+    }
+    return distance = distance * 264
+}
+
+function calculatesFarePrice(pickup, destination) {
+  let response;
+  let distance = distanceTravelledInFeet(pickup, destination);
+  if (distance <= 400) {
+    response = 0;
+  } else if (distance > 2500) {
+    response = 'cannot travel that far'
+  } else if (distance < 2000) {
+    response = (distance-400)*0.02
+  } else {
+    response = 25
+  }
+  return response
+}
