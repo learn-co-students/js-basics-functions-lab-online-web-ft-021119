249eaf716ffff55442d645224c1219ab95eda26a
AudreyJones <audrey.eliz.jones@gmail.com>
2019-05-23 09:44:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+// a block is 264 feet long.
+function distanceFromHqInBlocks(block) {
+    let distance;
+    distance = Math.abs(block - 42)
+    return distance;
+}
+
+function distanceFromHqInFeet(block) {
+    let distance;
+    distance = Math.abs(distanceFromHqInBlocks(block))
+    return distance * 264;
+}
+
+function distanceTravelledInFeet(start,end) {
+    let distance;
+    distance = Math.abs(end-start)
+    return distance * 264;
+}
+
+function calculatesFarePrice(start,end) {
+    let distance;
+    distance = distanceTravelledInFeet(start,end);
+    if (distance === 1) {
+        return 0
+      }
+      else if (distance >= 400) && (distance <= 2000) {
+        return (distance * 0.02)
+      }
+      else if (distance > 2000) && (distance < 2500) {
+        return 25;
+      }
+      else if (distance > 2500) {
+        return 'cannot travel that far';
+      }
+}
